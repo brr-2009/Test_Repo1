5fb82f40626a9d8ddb7a185b29b08433303bec11
brr-2009 <155716115+brr-2009@users.noreply.github.com>
2024-01-05 18:31:51 +0530

    Added more print statements

--- Sample_Project/src/main/Test.java
+++ Sample_Project/src/main/Test.java
@@ -2,5 +2,9 @@ public class Test{
 	public static void main(String args[]){
 	
 		System.out.println("Sample class");
+		System.out.println("added few more lines");
+		System.out.println("added few more lines");
+		System.out.println("added few more lines");
+		
 	}
 }
